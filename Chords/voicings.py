c65f962cf9054efd19e59f09aa00c8aebcde3c5f
Jawwwshh <j.mitchell430@gmail.com>
2025-08-30 11:53:58 +0100

    Add files via upload

---
+++ Chords/voicings.py
@@ -0,0 +1,316 @@
+    RAW_VOICINGS = """
+    C major root: C4 E4 G4
+    C major 1st inversion: E3 G3 C4
+    C major 2nd inversion: G3 C4 E4
+    C# major root: C#4 E#4 G#4
+    C# major 1st inversion: E#3 G#3 C#4
+    C# major 2nd inversion: G#3 C#4 E#4
+    D major root: D4 F#4 A4
+    D major 1st inversion: F#3 A3 D4
+    D major 2nd inversion: A3 D4 F#4
+    Eb major root: Eb4 G4 Bb4
+    Eb major 1st inversion: G3 Bb3 Eb4
+    Eb major 2nd inversion: Bb3 Eb4 G4
+    E major root: E4 G#4 B4
+    E major 1st inversion: G#3 B3 E4
+    E major 2nd inversion: B3 E4 G#4
+    F major root: F4 A4 C5
+    F major 1st inversion: A3 C4 F4
+    F major 2nd inversion: C4 F4 A4
+    F# major root: F#4 A#4 C#5
+    F# major 1st inversion: A#3 C#4 F#4
+    F# major 2nd inversion: C#4 F#4 A#4
+    G major root: G4 B4 D5
+    G major 1st inversion: B3 D4 G4
+    G major 2nd inversion: D4 G4 B4
+    Ab major root: Ab4 C5 Eb5
+    Ab major 1st inversion: C4 Eb4 Ab4
+    Ab major 2nd inversion: Eb4 Ab4 C5
+    A major root: A4 C#5 E5
+    A major 1st inversion: C#4 E4 A4
+    A major 2nd inversion: E4 A4 C#5
+    Bb major root: Bb4 D5 F5
+    Bb major 1st inversion: D4 F4 Bb4
+    Bb major 2nd inversion: F4 Bb4 D5
+    B major root: B4 D#5 F#5
+    B major 1st inversion: D#4 F#4 B4
+    B major 2nd inversion: F#4 B4 D#5
+    C minor root: C4 Eb4 G4
+    C minor 1st inversion: Eb3 G3 C4
+    C minor 2nd inversion: G3 C4 Eb4
+    C# minor root: C#4 E4 G#4
+    C# minor 1st inversion: E3 G#3 C#4
+    C# minor 2nd inversion: G#3 C#4 E4
+    D minor root: D4 F4 A4
+    D minor 1st inversion: F3 A3 D4
+    D minor 2nd inversion: A3 D4 F4
+    Eb minor root: Eb4 Gb4 Bb4
+    Eb minor 1st inversion: Gb3 Bb3 Eb4
+    Eb minor 2nd inversion: Bb3 Eb4 Gb4
+    E minor root: E4 G4 B4
+    E minor 1st inversion: G3 B3 E4
+    E minor 2nd inversion: B3 E4 G4
+    F minor root: F4 Ab4 C5
+    F minor 1st inversion: Ab3 C4 F4
+    F minor 2nd inversion: C4 F4 Ab4
+    F# minor root: F#4 A4 C#5
+    F# minor 1st inversion: A3 C#4 F#4
+    F# minor 2nd inversion: C#4 F#4 A4
+    G minor root: G4 Bb4 D5
+    G minor 1st inversion: Bb3 D4 G4
+    G minor 2nd inversion: D4 G4 Bb4
+    Ab minor root: Ab4 Cb5 Eb5
+    Ab minor 1st inversion: Cb4 Eb4 Ab4
+    Ab minor 2nd inversion: Eb4 Ab4 Cb5
+    A minor root: A4 C5 E5
+    A minor 1st inversion: C4 E4 A4
+    A minor 2nd inversion: E4 A4 C5
+    Bb minor root: Bb4 Db5 F5
+    Bb minor 1st inversion: Db4 F4 Bb4
+    Bb minor 2nd inversion: F4 Bb4 Db5
+    B minor root: B4 D5 F#5
+    B minor 1st inversion: D4 F#4 B4
+    B minor 2nd inversion: F#4 B4 D5
+    C diminished root: C4 Eb4 Gb4
+    C diminished 1st inversion: Eb3 Gb3 C4
+    C diminished 2nd inversion: Gb3 C4 Eb4
+    C# diminished root: C#4 E4 G4
+    C# diminished 1st inversion: E3 G3 C#4
+    C# diminished 2nd inversion: G3 C#4 E4
+    D diminished root: D4 F4 Ab4
+    D diminished 1st inversion: F3 Ab3 D4
+    D diminished 2nd inversion: Ab3 D4 F4
+    Eb diminished root: Eb4 Gb4 Bb4
+    Eb diminished 1st inversion: Gb3 Bb3 Eb4
+    Eb diminished 2nd inversion: Bb3 Eb4 Gb4
+    E diminished root: E4 G4 Bb4
+    E diminished 1st inversion: G3 Bb3 E4
+    E diminished 2nd inversion: Bb3 E4 G4
+    F diminished root: F4 Ab4 Cb5
+    F diminished 1st inversion: Ab3 Cb4 F4
+    F diminished 2nd inversion: Cb4 F4 Ab4
+    F# diminished root: F#4 A4 C5
+    F# diminished 1st inversion: A3 C4 F#4
+    F# diminished 2nd inversion: C4 F#4 A4
+    G diminished root: G4 Bb4 Db5
+    G diminished 1st inversion: Bb3 Db4 G4
+    G diminished 2nd inversion: Db4 G4 Bb4
+    Ab diminished root: Ab4 Cb5 Eb5
+    Ab diminished 1st inversion: Cb4 Eb4 Ab4
+    Ab diminished 2nd inversion: Eb4 Ab4 Cb5
+    A diminished root: A4 C5 Eb5
+    A diminished 1st inversion: C4 Eb4 A4
+    A diminished 2nd inversion: Eb4 A4 C5
+    Bb diminished root: Bb4 Db5 Fb5
+    Bb diminished 1st inversion: Db4 Fb4 Bb4
+    Bb diminished 2nd inversion: Fb4 Bb4 Db5
+    B diminished root: B4 D5 F5
+    B diminished 1st inversion: D4 F4 B4
+    B diminished 2nd inversion: F4 B4 D5
+    C major seventh root: C4 E4 G4 B4
+    C major seventh 1st inversion: E3 G3 B3 C4
+    C major seventh 2nd inversion: G3 B3 C4 E4
+    C major seventh 3rd inversion: B3 C4 E4 G4
+    C# major seventh root: C#4 E#4 G#4 B#4
+    C# major seventh 1st inversion: E#3 G#3 B#3 C#4
+    C# major seventh 2nd inversion: G#3 B#3 C#4 E#4
+    C# major seventh 3rd inversion: B#3 C#4 E#4 G#4
+    D major seventh root: D4 F#4 A4 C#5
+    D major seventh 1st inversion: F#3 A3 C#4 D4
+    D major seventh 2nd inversion: A3 C#4 D4 F#4
+    D major seventh 3rd inversion: C#4 D4 F#4 A4
+    Eb major seventh root: Eb4 G4 Bb4 D5
+    Eb major seventh 1st inversion: G3 Bb3 D4 Eb4
+    Eb major seventh 2nd inversion: Bb3 D4 Eb4 G4
+    Eb major seventh 3rd inversion: D4 Eb4 G4 Bb4
+    E major seventh root: E4 G#4 B4 D#5
+    E major seventh 1st inversion: G#3 B3 D#4 E4
+    E major seventh 2nd inversion: B3 D#4 E4 G#4
+    E major seventh 3rd inversion: D#4 E4 G#4 B4
+    F major seventh root: F4 A4 C5 E5
+    F major seventh 1st inversion: A3 C4 E4 F4
+    F major seventh 2nd inversion: C4 E4 F4 A4
+    F major seventh 3rd inversion: E4 F4 A4 C5
+    F# major seventh root: F#4 A#4 C#5 E#5
+    F# major seventh 1st inversion: A#3 C#4 E#4 F#4
+    F# major seventh 2nd inversion: C#4 E#4 F#4 A#4
+    F# major seventh 3rd inversion: E#4 F#4 A#4 C#5
+    G major seventh root: G4 B4 D5 F#5
+    G major seventh 1st inversion: B3 D4 F#4 G4
+    G major seventh 2nd inversion: D4 F#4 G4 B4
+    G major seventh 3rd inversion: F#4 G4 B4 D5
+    Ab major seventh root: Ab4 C5 Eb5 G5
+    Ab major seventh 1st inversion: C4 Eb4 G4 Ab4
+    Ab major seventh 2nd inversion: Eb4 G4 Ab4 C5
+    Ab major seventh 3rd inversion: G4 Ab4 C5 Eb5
+    A major seventh root: A4 C#5 E5 G#5
+    A major seventh 1st inversion: C#4 E4 G#4 A4
+    A major seventh 2nd inversion: E4 G#4 A4 C#5
+    A major seventh 3rd inversion: G#4 A4 C#5 E5
+    Bb major seventh root: Bb4 D5 F5 A5
+    Bb major seventh 1st inversion: D4 F4 A4 Bb4
+    Bb major seventh 2nd inversion: F4 A4 Bb4 D5
+    Bb major seventh 3rd inversion: A4 Bb4 D5 F5
+    B major seventh root: B4 D#5 F#5 A#5
+    B major seventh 1st inversion: D#4 F#4 A#4 B4
+    B major seventh 2nd inversion: F#4 A#4 B4 D#5
+    B major seventh 3rd inversion: A#4 B4 D#5 F#5
+    C dominant seventh root: C4 E4 G4 Bb4
+    C dominant seventh 1st inversion: E3 G3 Bb3 C4
+    C dominant seventh 2nd inversion: G3 Bb3 C4 E4
+    C dominant seventh 3rd inversion: Bb3 C4 E4 G4
+    C# dominant seventh root: C#4 E#4 G#4 B4
+    C# dominant seventh 1st inversion: E#3 G#3 B3 C#4
+    C# dominant seventh 2nd inversion: G#3 B3 C#4 E#4
+    C# dominant seventh 3rd inversion: B3 C#4 E#4 G#4
+    D dominant seventh root: D4 F#4 A4 C5
+    D dominant seventh 1st inversion: F#3 A3 C4 D4
+    D dominant seventh 2nd inversion: A3 C4 D4 F#4
+    D dominant seventh 3rd inversion: C4 D4 F#4 A4
+    Eb dominant seventh root: Eb4 G4 Bb4 Db5
+    Eb dominant seventh 1st inversion: G3 Bb3 Db4 Eb4
+    Eb dominant seventh 2nd inversion: Bb3 Db4 Eb4 G4
+    Eb dominant seventh 3rd inversion: Db4 Eb4 G4 Bb4
+    E dominant seventh root: E4 G#4 B4 D5
+    E dominant seventh 1st inversion: G#3 B3 D4 E4
+    E dominant seventh 2nd inversion: B3 D4 E4 G#4
+    E dominant seventh 3rd inversion: D4 E4 G#4 B4
+    F dominant seventh root: F4 A4 C5 Eb5
+    F dominant seventh 1st inversion: A3 C4 Eb4 F4
+    F dominant seventh 2nd inversion: C4 Eb4 F4 A4
+    F dominant seventh 3rd inversion: Eb4 F4 A4 C5
+    F# dominant seventh root: F#4 A#4 C#5 E5
+    F# dominant seventh 1st inversion: A#3 C#4 E4 F#4
+    F# dominant seventh 2nd inversion: C#4 E4 F#4 A#4
+    F# dominant seventh 3rd inversion: E4 F#4 A#4 C#5
+    G dominant seventh root: G4 B4 D5 F5
+    G dominant seventh 1st inversion: B3 D4 F4 G4
+    G dominant seventh 2nd inversion: D4 F4 G4 B4
+    G dominant seventh 3rd inversion: F4 G4 B4 D5
+    Ab dominant seventh root: Ab4 C5 Eb5 Gb5
+    Ab dominant seventh 1st inversion: C4 Eb4 Gb4 Ab4
+    Ab dominant seventh 2nd inversion: Eb4 Gb4 Ab4 C5
+    Ab dominant seventh 3rd inversion: Gb4 Ab4 C5 Eb5
+    A dominant seventh root: A4 C#5 E5 G5
+    A dominant seventh 1st inversion: C#4 E4 G4 A4
+    A dominant seventh 2nd inversion: E4 G4 A4 C#5
+    A dominant seventh 3rd inversion: G4 A4 C#5 E5
+    Bb dominant seventh root: Bb4 D5 F5 Ab5
+    Bb dominant seventh 1st inversion: D4 F4 Ab4 Bb4
+    Bb dominant seventh 2nd inversion: F4 Ab4 Bb4 D5
+    Bb dominant seventh 3rd inversion: Ab4 Bb4 D5 F5
+    B dominant seventh root: B4 D#5 F#5 A5
+    B dominant seventh 1st inversion: D#4 F#4 A4 B4
+    B dominant seventh 2nd inversion: F#4 A4 B4 D#5
+    B dominant seventh 3rd inversion: A4 B4 D#5 F#5
+    C minor seventh root: C4 Eb4 G4 Bb4
+    C minor seventh 1st inversion: Eb3 G3 Bb3 C4
+    C minor seventh 2nd inversion: G3 Bb3 C4 Eb4
+    C minor seventh 3rd inversion: Bb3 C4 Eb4 G4
+    C# minor seventh root: C#4 E4 G#4 B4
+    C# minor seventh 1st inversion: E3 G#3 B3 C#4
+    C# minor seventh 2nd inversion: G#3 B3 C#4 E4
+    C# minor seventh 3rd inversion: B3 C#4 E4 G#4
+    D minor seventh root: D4 F4 A4 C5
+    D minor seventh 1st inversion: F3 A3 C4 D4
+    D minor seventh 2nd inversion: A3 C4 D4 F4
+    D minor seventh 3rd inversion: C4 D4 F4 A4
+    Eb minor seventh root: Eb4 Gb4 Bb4 Db5
+    Eb minor seventh 1st inversion: Gb3 Bb3 Db4 Eb4
+    Eb minor seventh 2nd inversion: Bb3 Db4 Eb4 Gb4
+    Eb minor seventh 3rd inversion: Db4 Eb4 Gb4 Bb4
+    E minor seventh root: E4 G4 B4 D5
+    E minor seventh 1st inversion: G3 B3 D4 E4
+    E minor seventh 2nd inversion: B3 D4 E4 G4
+    E minor seventh 3rd inversion: D4 E4 G4 B4
+    F minor seventh root: F4 Ab4 C5 Eb5
+    F minor seventh 1st inversion: Ab3 C4 Eb4 F4
+    F minor seventh 2nd inversion: C4 Eb4 F4 Ab4
+    F minor seventh 3rd inversion: Eb4 F4 Ab4 C5
+    F# minor seventh root: F#4 A4 C#5 E5
+    F# minor seventh 1st inversion: A3 C#4 E4 F#4
+    F# minor seventh 2nd inversion: C#4 E4 F#4 A4
+    F# minor seventh 3rd inversion: E4 F#4 A4 C#5
+    G minor seventh root: G4 Bb4 D5 F5
+    G minor seventh 1st inversion: Bb3 D4 F4 G4
+    G minor seventh 2nd inversion: D4 F4 G4 Bb4
+    G minor seventh 3rd inversion: F4 G4 Bb4 D5
+    Ab minor seventh root: Ab4 Cb5 Eb5 Gb5
+    Ab minor seventh 1st inversion: Cb4 Eb4 Gb4 Ab4
+    Ab minor seventh 2nd inversion: Eb4 Gb4 Ab4 Cb5
+    Ab minor seventh 3rd inversion: Gb4 Ab4 Cb5 Eb5
+    A minor seventh root: A4 C5 E5 G5
+    A minor seventh 1st inversion: C4 E4 G4 A4
+    A minor seventh 2nd inversion: E4 G4 A4 C5
+    A minor seventh 3rd inversion: G4 A4 C5 E5
+    Bb minor seventh root: Bb4 Db5 F5 Ab5
+    Bb minor seventh 1st inversion: Db4 F4 Ab4 Bb4
+    Bb minor seventh 2nd inversion: F4 Ab4 Bb4 Db5
+    Bb minor seventh 3rd inversion: Ab4 Bb4 Db5 F5
+    B minor seventh root: B4 D5 F#5 A5
+    B minor seventh 1st inversion: D4 F#4 A4 B4
+    B minor seventh 2nd inversion: F#4 A4 B4 D5
+    B minor seventh 3rd inversion: A4 B4 D5 F#5
+    C minor seventh flat five root: C4 Eb4 Gb4 Bb4
+    C minor seventh flat five 1st inversion: Eb3 Gb3 Bb3 C4
+    C minor seventh flat five 2nd inversion: Gb3 Bb3 C4 Eb4
+    C minor seventh flat five 3rd inversion: Bb3 C4 Eb4 Gb4
+    C# minor seventh flat five root: C#4 E4 G4 B4
+    C# minor seventh flat five 1st inversion: E3 G3 B3 C#4
+    C# minor seventh flat five 2nd inversion: G3 B3 C#4 E4
+    C# minor seventh flat five 3rd inversion: B3 C#4 E4 G4
+    D minor seventh flat five root: D4 F4 Ab4 C5
+    D minor seventh flat five 1st inversion: F3 Ab3 C4 D4
+    D minor seventh flat five 2nd inversion: Ab3 C4 D4 F4
+    D minor seventh flat five 3rd inversion: C4 D4 F4 Ab4
+    Eb minor seventh flat five root: Eb4 Gb4 Bb4 Db5
+    Eb minor seventh flat five 1st inversion: Gb3 Bb3 Db4 Eb4
+    Eb minor seventh flat five 2nd inversion: Bb3 Db4 Eb4 Gb4
+    Eb minor seventh flat five 3rd inversion: Db4 Eb4 Gb4 Bb4
+    E minor seventh flat five root: E4 G4 Bb4 D5
+    E minor seventh flat five 1st inversion: G3 Bb3 D4 E4
+    E minor seventh flat five 2nd inversion: Bb3 D4 E4 G4
+    E minor seventh flat five 3rd inversion: D4 E4 G4 Bb4
+    F minor seventh flat five root: F4 Ab4 Cb5 Eb5
+    F minor seventh flat five 1st inversion: Ab3 Cb4 Eb4 F4
+    F minor seventh flat five 2nd inversion: Cb4 Eb4 F4 Ab4
+    F minor seventh flat five 3rd inversion: Eb4 F4 Ab4 Cb5
+    F# minor seventh flat five root: F#4 A4 C5 E5
+    F# minor seventh flat five 1st inversion: A3 C4 E4 F#4
+    F# minor seventh flat five 2nd inversion: C4 E4 F#4 A4
+    F# minor seventh flat five 3rd inversion: E4 F#4 A4 C5
+    G minor seventh flat five root: G4 Bb4 Db5 F5
+    G minor seventh flat five 1st inversion: Bb3 Db4 F4 G4
+    G minor seventh flat five 2nd inversion: Db4 F4 G4 Bb4
+    G minor seventh flat five 3rd inversion: F4 G4 Bb4 Db5
+    Ab minor seventh flat five root: Ab4 Cb5 Eb5 Gb5
+    Ab minor seventh flat five 1st inversion: Cb4 Eb4 Gb4 Ab4
+    Ab minor seventh flat five 2nd inversion: Eb4 Gb4 Ab4 Cb5
+    Ab minor seventh flat five 3rd inversion: Gb4 Ab4 Cb5 Eb5
+    A minor seventh flat five root: A4 C5 Eb5 G5
+    A minor seventh flat five 1st inversion: C4 Eb4 G4 A4
+    A minor seventh flat five 2nd inversion: Eb4 G4 A4 C5
+    A minor seventh flat five 3rd inversion: G4 A4 C5 Eb5
+    Bb minor seventh flat five root: Bb4 Db5 Fb5 Ab5
+    Bb minor seventh flat five 1st inversion: Db4 Fb4 Ab4 Bb4
+    Bb minor seventh flat five 2nd inversion: Fb4 Ab4 Bb4 Db5
+    Bb minor seventh flat five 3rd inversion: Ab4 Bb4 Db5 Fb5
+    B minor seventh flat five root: B4 D5 F5 A5
+    B minor seventh flat five 1st inversion: D4 F4 A4 B4
+    B minor seventh flat five 2nd inversion: F4 A4 B4 D5
+    B minor seventh flat five 3rd inversion: A4 B4 D5 F5
+    """
+
+def parse_voicings(raw_text):
+    voicings = {}
+    for line in raw_text.strip().splitlines():
+        if not line.strip():
+            continue
+        if ":" not in line:
+            continue
+        name, notes = line.split(":", 1)
+        voicings[name.strip()] = notes.strip().split()
+    return voicings
+
+CHORD_VOICINGS = parse_voicings(RAW_VOICINGS)
+
